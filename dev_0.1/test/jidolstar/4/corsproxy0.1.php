32b320ee9dadebb7dac5393b1b03bad7d20b6dbc
'jidolstar' <'jidolstar@cookilab.com'>
2014-06-07 22:59:23 +0900

    curl 함수 중복제거

--- dev_0.1/test/jidolstar/4/corsproxy0.1.php
+++ dev_0.1/test/jidolstar/4/corsproxy0.1.php
@@ -4,7 +4,16 @@ header('Access-Control-Max-Age: 5');
 header('Access-Control-Allow-Methods: POST, OPTIONS');   
 header('Access-Control-Allow-header: Content-Type, Cache-Control');       
 header('Access-Control-Allow-Credentials: true');
-header("Content-Type: application/x-www-form-urlencoded;charset=utf-8");   
+
+error_reporting( E_ALL );
+ini_set( 'display_errors', 0 );
+ini_set( 'log_errors', 1 );
+ini_set( 'url_rewriter.tags', '' );
+ini_set( 'session.use_trans_sid', 0 );
+register_shutdown_function( '__shutdown_handler' );
+set_exception_handler( '__exception_handler' );
+set_error_handler( '__error_handler' );
+
 if( count($_POST) == 0 && isset($HTTP_RAW_POST_DATA) ){
 	$data = explode('&', $HTTP_RAW_POST_DATA);
 	for( $i = 0, $j = count($data); $i < $j; ++$i ){
@@ -15,18 +24,11 @@ if( count($_POST) == 0 && isset($HTTP_RAW_POST_DATA) ){
 		}
 	}
 }
+
 if( !isset($_POST['url']) || !isset($_POST['method']) || !isset($_POST['data']) || !isset($_POST['header'] ) || !isset($_POST['key']) || !isset($_POST['cookie']) ) {
 	__error('Wrong parameters');
 	exit;
 }
-error_reporting( E_ALL );
-ini_set( 'display_errors', 0 );
-ini_set( 'log_errors', 1 );
-ini_set( 'url_rewriter.tags', '' );
-ini_set( 'session.use_trans_sid', 0 );
-register_shutdown_function( '__shutdown_handler' );
-set_exception_handler( '__exception_handler' );
-set_error_handler( '__error_handler' );
 
 $url = $_POST['url'];
 $method = $_POST['method'];
@@ -37,63 +39,23 @@ foreach( $temps as $k => $v ) $header[] = $k.": ".$v;
 $data = $_POST['data'];
 
 switch( $method ) {
-case 'GET': $ret = __get( $url, $header, $cookie ); break;
-case 'POST': $ret = __post( $url, $header, $cookie, $data ); break;
-case 'PUT': $ret = __put( $url, $header, $cookie, $data ); break;
-case 'DELETE': $ret = __delete( $url, $header, $cookie, $data ); break;
-default; $ret = ''; break;
+case 'GET': echo __curl( $url, $header, $cookie, '', CURLOPT_POST, FALSE ); break;
+case 'POST': echo __curl( $url, $header, $cookie, $data, CURLOPT_POST, TRUE ); break;
+case 'PUT': echo __curl( $url, $header, $cookie, $data, CURLOPT_POST, TRUE, CURLOPT_CUSTOMREQUEST, $method ); break;
+case 'DELETE': echo __curl( $url, $header, $cookie, $data, CURLOPT_POST, TRUE, CURLOPT_CUSTOMREQUEST, $method ); break;
 }
-echo $ret;
 exit;
-function __get( $url, $header, $cookie ){
+function __curl( $url, $header, $cookie, $data ){
 	$t0 = curl_init();
 	curl_setopt( $t0, CURLOPT_HEADER, FALSE );
 	curl_setopt( $t0, CURLOPT_RETURNTRANSFER, TRUE );
-	curl_setopt( $t0, CURLOPT_POST, FALSE );
 	curl_setopt( $t0, CURLOPT_URL, $url );
-	curl_setopt( $t0, CURLOPT_COOKIE, $cookie );
+	for( $i = 4, $args = func_get_args(), $cnt = count($args); $i < $cnt; ){
+		curl_setopt( $t0, $args[$i++], $args[$i++] );
+	}
+	if( $cookie && strlen($cookie) > 0 ) curl_setopt( $t0, CURLOPT_COOKIE, $cookie );
 	if( $header && count($header) > 0 ) curl_setopt( $t0, CURLOPT_HTTPHEADER, $header ); 
-	$t1 = curl_exec( $t0 );
-	curl_close( $t0 );
-	return $t1 === FALSE ? curl_error( $t0 ) : $t1;
-}
-function __post( $url, $header, $cookie, $data ){
-	$t0 = curl_init();
-	curl_setopt( $t0, CURLOPT_HEADER, FALSE );
-	curl_setopt( $t0, CURLOPT_RETURNTRANSFER, TRUE );
-	curl_setopt( $t0, CURLOPT_POST, TRUE );
-	curl_setopt( $t0, CURLOPT_URL, $url );
-	curl_setopt( $t0, CURLOPT_COOKIE, $cookie );
-	if( $header && count($header) > 0 ) curl_setopt( $t0, CURLOPT_HTTPHEADER, $header );
-	if( $data ) curl_setopt( $t0, CURLOPT_POSTFIELDS, $data );
-	$t1 = curl_exec( $t0 );
-	curl_close( $t0 );
-	return $t1 === FALSE ? curl_error( $t0 ) : $t1;
-}
-function __put( $url, $header, $cookie, $data ){
-	$t0 = curl_init();
-	curl_setopt( $t0, CURLOPT_CUSTOMREQUEST, "PUT" );
-	curl_setopt( $t0, CURLOPT_HEADER, FALSE );
-	curl_setopt( $t0, CURLOPT_RETURNTRANSFER, TRUE );
-	curl_setopt( $t0, CURLOPT_POST, TRUE );
-	curl_setopt( $t0, CURLOPT_URL, $url );
-	curl_setopt( $t0, CURLOPT_COOKIE, $cookie );
-	if( $header && count($header) > 0 ) curl_setopt( $t0, CURLOPT_HTTPHEADER, $header );
-	if( $data ) curl_setopt( $t0, CURLOPT_POSTFIELDS, $data );
-	$t1 = curl_exec( $t0 );
-	curl_close( $t0 );
-	return $t1 === FALSE ? curl_error( $t0 ) : $t1;
-}
-function __delete( $url, $header, $cookie, $data ){
-	$t0 = curl_init();
-	curl_setopt( $t0, CURLOPT_CUSTOMREQUEST, "DELETE" );
-	curl_setopt( $t0, CURLOPT_HEADER, FALSE );
-	curl_setopt( $t0, CURLOPT_RETURNTRANSFER, TRUE );
-	curl_setopt( $t0, CURLOPT_POST, TRUE );
-	curl_setopt( $t0, CURLOPT_URL, $url );
-	curl_setopt( $t0, CURLOPT_COOKIE, $cookie );
-	if( $header && count($header) > 0 ) curl_setopt( $t0, CURLOPT_HTTPHEADER, $header );
-	if( $data ) curl_setopt( $t0, CURLOPT_POSTFIELDS, $data );
+	if( $data && strlen($data) > 0 ) curl_setopt( $t0, CURLOPT_POSTFIELDS, $data );
 	$t1 = curl_exec( $t0 );
 	curl_close( $t0 );
 	return $t1 === FALSE ? curl_error( $t0 ) : $t1;
