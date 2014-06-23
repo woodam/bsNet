033b4411e404a9b9c47cc708c550c0fd3078fb0d
hikaMaeng <hika00@gmail.com>
2014-06-23 11:00:39 +0900

    컨벤션 수정 cors->cross

--- bsNet.0.1.js
+++ bsNet.0.1.js
@@ -6,15 +6,15 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPR
 		while( i-- ){try{new ActiveXObject( j = t0[i] );}catch(e){continue;}break;}
 		return function(){return new ActiveXObject(j);};
 	})() : function(){return new XMLHttpRequest;},
-	cors = W['XDomainRequest'] ? (function(){
+	cross = W['XDomainRequest'] ? (function(){
 		var mk = function( x, err ){return function(){x.ontimeout = x.onload = x.onerror = null, err ? ( x.abort(), end( null, err ) ) : end( x.responseText, x.contentType );};};
 		return function( data, end ){
 			var x = new XDomainRequest;
-			x.ontimeout = mk( x, 'timeout'), x.timeout = timeout, x.onerror = mk( x, 'xdr error'), x.onload = mk( x, time), x.open( 'POST', CROSSPROXY ), x.send(data);
+			x.ontimeout = mk( x, 'timeout'), x.timeout = timeout, x.onerror = mk( x, 'xdr error'), x.onload = mk( x, time ), x.open( 'POST', CROSSPROXY ), x.send(data);
 		};
 	})() : W['XMLHttpRequest'] ? function( data, end ){
 		var x = xhr();
-		async( x, end ), x.open( 'POST', CROSSPROXY, true ),	x.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' ), x.withCredentials = true, x.send(data);
+		async( x, end ), x.open( 'POST', CROSSPROXY, true ), x.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' ), x.withCredentials = true, x.send(data);
 	} : 0,
 	url = function( U, arg ){
 		var t0 = U.replace( trim, '' ).split('#'), p = param( arg, 2 );
@@ -39,19 +39,19 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPR
 	param = function( arg, i ){
 		var t0, j, k, v, m;
 		if( !arg || ( j = arg.length ) < i + 1 ) return '';
-		head.corsKey = head.length = paramBody.length = 0;
+		head.crossKey = head.length = paramBody.length = 0;
 		while( i < j ){
 			if( !( k = arg[i++].replace( trim, '' ) ).length ) err(5005);
 			if( i < j ){
 				v = arg[i++],
 				k.charAt(0) === '@' ? head.push( k.substr(1), paramHeader(v) ) :
-				k == 'corsAccessKey' ? head.corsKey = v :
+				k == 'crossAccessKey' ? head.crossKey = v :
 					paramBody[paramBody.length] = encodeURIComponent(k) + '=' + encodeURIComponent(( v && typeof v == 'object' ? JSON.stringify(v) : v + '' ).replace( trim, '' ));
 			}else m = encodeURIComponent(k);
 		}
 		return m || paramBody.join('&');
 	},
-	httpCors = cors ? [] : 0, httpH = [], httpMethod,
+	httpCross = cross ? [] : 0, httpH = [], httpMethod,
 	http = function( method, end, U, arg ){
 		var x, key, i, j, k;
 		if( ( httpMethod = method ) === 'GET' ){
@@ -59,14 +59,14 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPR
 			arg = '';
 		}else U = url(U), arg = param( arg, 2 );
 		if( ( i = U.indexOf( '://' ) ) > -1 && U.substr( i + 3, ( j = location.hostname).length ) != j ){
-			if( !end || !cors ) return err(5001);
-			for( key = head.corsKey || CROSSPROXYKEY, httpCors.length = httpH.length = i = 0, j = head.length ; i < j ; i += 2 )
-				baseHeader[httpCors[i] = k = head[i]] ? httpH[httpH.length] = k : 0, httpCors[i + 1] = head[i + 1];
+			if( !end || !cross ) return err(5001);
+			for( key = head.crossKey || CROSSPROXYKEY, httpCross.length = httpH.length = i = 0, j = head.length ; i < j ; i += 2 )
+				baseHeader[httpCross[i] = k = head[i]] ? httpH[httpH.length] = k : 0, httpCross[i + 1] = head[i + 1];
 			k = i;
-			for( i in baseHeader ) if( httpH.indexOf(i) == -1 ) httpCors[k++] = i, httpCors[k++] = paramHeader(baseHeader[i]);
-			k = param( httpCors, 0 ), httpCors.length = 0,
-			httpCors.push( 'url', U, 'method', method, 'key', key, 'cookie', document.cookie, 'data', arg, 'header', k );
-			cors( param(httpCors, 0), end );
+			for( i in baseHeader ) if( httpH.indexOf(i) == -1 ) httpCross[k++] = i, httpCross[k++] = paramHeader(baseHeader[i]);
+			k = param( httpCross, 0 ), httpCross.length = 0,
+			httpCross.push( 'url', U, 'method', method, 'key', key, 'cookie', document.cookie, 'data', arg, 'header', k );
+			cross( param(httpCross, 0), end );
 		}else{
 			x = xhr();
 			if( end ) async( x, end );
