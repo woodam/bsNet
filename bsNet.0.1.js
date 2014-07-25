29d7161e6fd27fc2a1e19594d5e56b0a0cb27f6c
'jidolstar' <'jidolstar@cookilab.com'>
2014-07-25 11:27:21 +0900

    버그 Fix 및 최신화 작업

--- bsNet.0.1.js
+++ bsNet.0.1.js
@@ -3,24 +3,28 @@
  * http://www.bsplugin.com All rights reserved.
  * Licensed under the BSD license. See http://opensource.org/licenses/BSD-3-Clause
  */
-var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPROXYKEY = 'CROSSPROXY_DEMO_ACCESS_KEY', bsNet = {};
-(function( trim, detect, fn, mk ){
-	var xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (function(){
+'use strict';
+if( !this['console'] ) this['console'] = {log:function(){}};
+var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPROXYKEY = 'CROSSPROXY_DEMO_ACCESS_KEY', bsNet = {}, isDebug = 0,
+	err = function( num, msg ){console.log( num, msg ); if( isDebug ) throw new Error( num, msg );}, 
+	bs = bs || {}, timeout = 5000, fn = function( k, v ){bsNet[k] = v;};
+	bs['rand'] || (function(){
+		var rc, rand = function(){return rc = ( ++rc ) % 1000, rand[rc] || ( rand[rc] = Math.random() );};
+		bs['rand'] = function( a, b ){ return parseInt( rand() * ( b - a + 1 ) ) + a; };
+	})();
+(function( W, trim, detect, fn, mk ){
+	var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (function(){
 		var t0, i, j;
+		xhrType = 1;
 		t0 = 'MSXML2.XMLHTTP', t0 = ['Microsoft.XMLHTTP',t0,t0+'.3.0',t0+'.4.0',t0+'.5.0'], i = t0.length;
 		while( i-- ){try{new ActiveXObject( j = t0[i] );}catch(e){continue;}break;}
 		return function(){return new ActiveXObject(j);};
 	})() : function(){return new XMLHttpRequest;},
 	cross = W['XDomainRequest'] ? (function(){
-		var mk = function( x, err ){
-			return function(){
-				var v;
-				x.ontimeout = x.onload = x.onerror = null, err ? ( x.abort(), end( null, err ) ) : end(x.responseText);//( v = JSON.parse(x.responseText), end( v.data, v.header ) );
-			};
-		};
+		var mk = function( x, err, end ){return function(){x.ontimeout = x.onload = x.onerror = null, err ? ( x.abort(), end( null, err ) ) : end(x.responseText);};};
 		return function( data, end ){
 			var x = new XDomainRequest;
-			x.ontimeout = mk( x, 'timeout'), x.timeout = timeout, x.onerror = mk( x, 'xdr error'), x.onload = mk(x), x.open( 'POST', CROSSPROXY ), x.send(data);
+			x.ontimeout = mk( x, 'timeout', end ), x.timeout = timeout, x.onerror = mk( x, 'xdr error', end ), x.onload = mk( x, 0, end ), x.open( 'POST', CROSSPROXY ), x.send(data);
 		};
 	})() : W['XMLHttpRequest'] ? function( data, end ){
 		var x = xhr();
@@ -40,9 +44,10 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPR
 			var text, status;
 			if( x.readyState !== 4 || timeId == -1 ) return;
 			clearTimeout(timeId), timeId = -1,
-			text = x.status === 200 || x.status === 0 ? x.responseText : null,
+			text = x.status == 200 || x.status == 0 ? x.responseText : null,
 			status = text ? x.getAllResponseHeaders() : x.status,
-			x.onreadystatechange = null, end( text, status );
+			xhrType ? delete x.onreadystatechange : x.onreadystatechange = null,
+			end( text, status );
 		};
 	},
 	head = [], paramBody = [], paramHeader = function(v){return typeof v == 'function' ? v(httpMethod) : v;},
@@ -75,7 +80,7 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPR
 			k = i;
 			for( i in baseHeader ) if( httpH.indexOf(i) == -1 ) httpCross[k++] = i, httpCross[k++] = paramHeader(baseHeader[i]);
 			k = param( httpCross, 0 ), httpCross.length = 0,
-			httpCross.push( 'url', U, 'method', method, 'key', key, 'cookie', document.cookie, 'data', arg, 'header', k );
+			httpCross.push( 'url', U, 'method', method, 'key', key, 'data', arg, 'header', k );
 			cross( param(httpCross, 0), end );
 		}else{
 			x = xhr();
@@ -88,10 +93,13 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.1.php', CROSSPR
 			}
 			for( i in baseHeader ) if( httpH.indexOf(i) == -1 ) x.setRequestHeader( i, paramHeader(baseHeader[i]) );
 			x.send(arg);
-			if( !end ) return i = x.responseText, x.onreadystatechange = null, i;
+			if( !end ) return ( i = x.responseText ), xhrType ? delete x.onreadystatechange : x.onreadystatechange = null, i;
 		}
 	}, baseHeader = {};
 	fn( 'header', function( k, v ){baseHeader[k] ? err( 2200, k ) : baseHeader[k] = v;} ),
 	mk = function(m){return function( end, url ){return http( m, end, url, arguments );};},
 	fn( 'post', mk('POST') ), fn( 'put', mk('PUT') ), fn( 'delete', mk('DELETE') ), fn( 'get', mk('GET') );
-})( /^\s*|\s*$/g,  {browser:W['XMLHttpRequest'] ? '!ie' : 'ie', browserVer:7}, function( k, v ){bsNet[k] = v;} );
+})( this, /^\s*|\s*$/g,  {browser:this['XMLHttpRequest'] ? '!ie' : 'ie', browserVer:7}, fn );
+fn = bsNet.header,
+fn( 'Cache-Control', 'no-cache' ),
+fn( 'Content-Type', function(type){return ( type == 'GET' ? 'text/plain' : 'application/x-www-form-urlencoded' ) + '; charset=UTF-8';} );
