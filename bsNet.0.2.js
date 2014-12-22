e03e051f40e8f1f0f5d7a1af4bc872aff08c852f
Isaac G <jidolstar@cookilab.com>
2014-12-22 23:17:32 +0900

    Proxy php 경로 수정 및 Blob, File 데이타 보내기 적용

--- bsNet.0.2.js
+++ bsNet.0.2.js
@@ -5,7 +5,7 @@
  */
 'use strict';
 if( !this['console'] ) this['console'] = {log:function(){}};
-var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.2.php',  
+var CROSSPROXY = 'http://api.bsplugin.com/bsNet/',  
 	CROSSPROXYKEY = 'CROSSPROXY_DEMO_ACCESS_KEY', bsNet = {}, isDebug = 0,
 	err = function( num, msg ){console.log( num, msg ); if( isDebug ) throw new Error( num, msg );}, 
 	bs = bs || {}, timeout = 5000, fn = function( k, v ){bsNet[k] = v;};
@@ -14,7 +14,7 @@ var CROSSPROXY = 'http://api.bsplugin.com/bsNet/php/crossProxy.0.2.php',
 		bs['rand'] = function( a, b ){ return parseInt( rand() * ( b - a + 1 ) ) + a; };
 	})();
 (function( W, trim, detect, fn, mk ){
-var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (function(){
+	var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (function(){
 		var t0, i, j;
 		xhrType = 1;
 		t0 = 'MSXML2.XMLHTTP', t0 = ['Microsoft.XMLHTTP',t0,t0+'.3.0',t0+'.4.0',t0+'.5.0'], i = t0.length;
@@ -35,7 +35,7 @@ var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (funct
 		var t0 = U.replace( trim, '' ).split('#'), p = param( arg, 2 );
 		return t0[0] + ( t0[0].indexOf('?') > -1 ? '&' : '?' ) + 'bsNC=' + bs.rand( 1000, 9999 ) + ( p ? '&' + p : '' ) + ( t0[1] ? '#' + t0[1] : '' );
 	},
-	async = function( x, end ){
+	async = function( x, end, arg ){
 		var timeId = setTimeout( function(){
 			if( timeId == -1 ) return;
 			if( x.readyState !== 4 ) x.abort();
@@ -45,13 +45,11 @@ var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (funct
 			var text, status;
 			if( x.readyState !== 4 || timeId == -1 ) return;
 			clearTimeout(timeId), timeId = -1,
-			text = x.status == 200 || x.status == 0 ? x.responseText : null,
-			status = text ? x.getAllResponseHeaders() : x.status,
 			xhrType ? delete x.onreadystatechange : x.onreadystatechange = null,
-			end( text, status );
+			end.call( xhr, x.responseText, x.status, arg );
 		};
 	},
-	head = [], paramBody = [], paramHeader = function(v){return typeof v == 'function' ? v(httpMethod) : v;},
+	head = [], paramBody = [], ck, paramHeader = function(v){return typeof v == 'function' ? v(httpMethod) : v;},
 	param = function( arg, i ){
 		var t0, j, k, v, m;
 		if( !arg || ( j = arg.length ) < i + 1 ) return '';
@@ -60,20 +58,46 @@ var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (funct
 			if( !( k = arg[i++].replace( trim, '' ) ).length ) err(5005);
 			if( i < j ){
 				v = arg[i++],
-				k.charAt(0) === '@' ? (console.log('head'),head.push( k.substr(1), paramHeader(v) )) :
+				k.charAt(0) === '@' ? head.push( k.substr(1), paramHeader(v) ) :
 				k == 'crossAccessKey' ? head.crossKey = v :
+				k == 'crossCookie' ? ( ck = encodeURIComponent(v) ) :
+				k == 'fullBody' ? m = v :
 					paramBody[paramBody.length] = encodeURIComponent(k) + '=' + encodeURIComponent(( v && typeof v == 'object' ? JSON.stringify(v) : v + '' ).replace( trim, '' ));
 			}else m = encodeURIComponent(k);
 		}
 		return m || paramBody.join('&');
 	},
 	httpCross = cross ? [] : 0, httpH = [], httpMethod,
+	makePostBoundary = function(){
+		var chars, res, rand, i, t0;
+		chars = [[48,57], [97,122]], res = '', rand = bs.rand, i = 12;
+		while(i--) t0 = chars[rand( 0, 1 )], res += String.fromCharCode( rand( t0[0], t0[1] ) );
+		return '------------' + res;
+	},
 	http = function( method, end, U, arg ){
-		var x, key, i, j, k;
-		if( ( httpMethod = method ) === 'GET' ){
+		var x, key, i, j, k, v, postBoundary, postBody;
+		if( ( httpMethod = method ) !== 'GET' && detect.xhr2 ){
+			i = arg.length;
+			while(i--) if( arg[i] instanceof File || arg[i] instanceof Blob ){ postBody = []; break; }
+			if( postBody ){
+				postBoundary = makePostBoundary(), i = 2, j = arg.length;
+				while(i < j){
+					k = arg[i++].replace( trim, '' ), v = arg[i++];
+					if( k.charAt(0) === '@' ) continue;
+					postBody.push( '--' + postBoundary + '\r\n' );
+					if( v instanceof File || v instanceof Blob )
+						postBody.push( 'Content-Disposition: form-data; name="' + k + '"; filename="' + ( v.name || 'bsPost' + (i/2) + '.bin' ) + '"\r\n' ),
+						postBody.push( 'Content-Type: application/octet-stream\r\n\r\n' );
+					else postBody.push( 'Content-Disposition: form-data; name="' + k + '"\r\n\r\n' );
+					postBody.push( v ), postBody.push( '\r\n' );
+				}
+				postBody.push( '--' + postBoundary );
+			}
+		}else postBody = null;
+		if( httpMethod === 'GET' ){
 			if( ( U = url( U, arg ) ).length > 512 ) err( 5004, U );
 			arg = '';
-		}else U = url(U), arg = param( arg, 2 );
+		}else U = url(U), ck = '', arg = param( arg, 2 );
 		if( ( i = U.indexOf( '://' ) ) > -1 && U.substr( i + 3, ( j = location.hostname).length ) != j ){
 			if( !end || !cross ) return err(5001);
 			for( key = head.crossKey || CROSSPROXYKEY, httpCross.length = httpH.length = i = 0, j = head.length ; i < j ; i += 2 )
@@ -81,11 +105,12 @@ var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (funct
 			k = i;
 			for( i in baseHeader ) if( httpH.indexOf(i) == -1 ) httpCross[k++] = i, httpCross[k++] = paramHeader(baseHeader[i]);
 			k = param( httpCross, 0 ), httpCross.length = 0,
-			httpCross.push( 'url', U, 'method', method, 'key', key, 'data', arg, 'header', k );
+			httpCross.push( 'url', U, 'cookie', ck, 'method', method, 'key', key, 'data', arg, 'header', k );
 			cross( param(httpCross, 0), end );
 		}else{
 			x = xhr();
-			if( end ) async( x, end );
+			if( end ) async( x, end, arg );
+			if( postBody ) head.push("Content-Type", "multipart/form-data; boundary=" + postBoundary);
 			x.open( method, U, end ? true : false ),
 			httpH.length = i = 0, j = head.length;
 			while( i < j ){
@@ -93,7 +118,8 @@ var xhrType = 0, xhr = detect.browser === 'ie' && detect.browserVer < 9 ? (funct
 				if( baseHeader[k] ) httpH[httpH.length] = k;
 			}
 			for( i in baseHeader ) if( httpH.indexOf(i) == -1 ) x.setRequestHeader( i, paramHeader(baseHeader[i]) );
-			x.send(arg);
+			if( postBody ) x.send( new Blob(postBody) );
+			else x.send(arg);
 			if( !end ) return ( i = x.responseText ), xhrType ? delete x.onreadystatechange : x.onreadystatechange = null, i;
 		}
 	}, baseHeader = {};
